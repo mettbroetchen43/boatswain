ddff6624f47d5cd02053419aba57242317f582bf
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2022-05-07 11:38:15 -0300

    plugins/obs-studio: Port to entry rows
    Use AdwEntryRow for the host entry, and password row for the
    password.

--- src/plugins/obs-studio/obs-connection-settings.c
+++ src/plugins/obs-studio/obs-connection-settings.c
@@ -24,12 +24,10 @@ struct _ObsConnectionSettings
 {
   AdwPreferencesGroup parent_instance;
 
-  GtkEditable *host_entry;
-  GtkEditable *password_entry;
-  GtkWidget *password_row;
+  GtkEditable *host_row;
+  GtkEditable *password_row;
   GtkAdjustment *port_adjustment;
 
-  guint authenticate_timeout_id;
   guint update_connection_timeout_id;
 
   ObsConnectionManager *connection_manager;
@@ -67,7 +65,7 @@ static void
 authenticate_connection (ObsConnectionSettings *self)
 {
   obs_connection_authenticate (self->connection,
-                               gtk_editable_get_text (self->password_entry),
+                               gtk_editable_get_text (self->password_row),
                                NULL,
                                on_connection_authenticated_cb,
                                self);
@@ -81,15 +79,14 @@ update_password_row (ObsConnectionSettings *self)
     case OBS_CONNECTION_STATE_DISCONNECTED:
     case OBS_CONNECTION_STATE_CONNECTING:
     case OBS_CONNECTION_STATE_CONNECTED:
-      g_clear_handle_id (&self->authenticate_timeout_id, g_source_remove);
-      gtk_widget_hide (self->password_row);
+      gtk_widget_hide (GTK_WIDGET (self->password_row));
       break;
 
     case OBS_CONNECTION_STATE_AUTHENTICATING:
       break;
 
     case OBS_CONNECTION_STATE_WAITING_FOR_CREDENTIALS:
-      gtk_widget_show (self->password_row);
+      gtk_widget_show (GTK_WIDGET (self->password_row));
       break;
     }
 }
@@ -119,7 +116,7 @@ update_connection (ObsConnectionSettings *self)
   g_autoptr (GError) error = NULL;
 
   connection = obs_connection_manager_get (self->connection_manager,
-                                           gtk_editable_get_text (self->host_entry),
+                                           gtk_editable_get_text (self->host_row),
                                            gtk_adjustment_get_value (self->port_adjustment),
                                            &error);
 
@@ -152,19 +149,8 @@ on_connection_authenticated_cb (GObject      *source_object,
     {
       if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_PROXY_AUTH_FAILED))
         g_warning ("Error authenticating connection: %s", error->message);
-      gtk_widget_add_css_class (GTK_WIDGET (self->password_entry), "error");
+      gtk_widget_add_css_class (GTK_WIDGET (self->password_row), "error");
     }
-}
-
-static gboolean
-authenticate_after_timeout_cb (gpointer data)
-{
-  ObsConnectionSettings *self = OBS_CONNECTION_SETTINGS (data);
-
-  authenticate_connection (self);
-
-  self->authenticate_timeout_id = 0;
-  return G_SOURCE_REMOVE;
 }
 
 static gboolean
@@ -188,33 +174,18 @@ on_connection_state_changed_cb (ObsConnection         *connection,
 }
 
 static void
-on_host_entry_text_changed_cb (GtkEditable           *host_entry,
-                               GParamSpec            *pspec,
-                               ObsConnectionSettings *self)
+on_host_row_applied_cb (GtkEditable           *host_row,
+                        ObsConnectionSettings *self)
 {
   g_clear_handle_id (&self->update_connection_timeout_id, g_source_remove);
-  self->update_connection_timeout_id = g_timeout_add_seconds (1,
-                                                              update_connection_after_timeout_cb,
-                                                              self);
+  update_connection (self);
 }
 
 static void
-on_password_entry_activated_cb (GtkEditable           *host_entry,
-                                ObsConnectionSettings *self)
+on_password_row_applied_cb (GtkEditable           *row,
+                            ObsConnectionSettings *self)
 {
-  g_clear_handle_id (&self->authenticate_timeout_id, g_source_remove);
   authenticate_connection (self);
-}
-
-static void
-on_password_entry_text_changed_cb (GtkEditable           *host_entry,
-                                   GParamSpec            *pspec,
-                                   ObsConnectionSettings *self)
-{
-  gtk_widget_remove_css_class (GTK_WIDGET (self->password_entry), "error");
-
-  g_clear_handle_id (&self->authenticate_timeout_id, g_source_remove);
-  self->authenticate_timeout_id = g_timeout_add_seconds (2, authenticate_after_timeout_cb, self);
 }
 
 static void
@@ -241,12 +212,8 @@ obs_connection_settings_finalize (GObject *object)
   if (self->update_connection_timeout_id > 0)
     update_connection (self);
 
-  if (self->authenticate_timeout_id > 0)
-    authenticate_connection (self);
-
   g_clear_signal_handler (&self->state_changed_id, self->connection);
   g_clear_handle_id (&self->update_connection_timeout_id, g_source_remove);
-  g_clear_handle_id (&self->authenticate_timeout_id, g_source_remove);
   g_clear_object (&self->connection_manager);
   g_clear_object (&self->connection);
 
@@ -260,9 +227,7 @@ obs_connection_settings_constructed (GObject *object)
 
   G_OBJECT_CLASS (obs_connection_settings_parent_class)->constructed (object);
 
-  g_signal_handlers_block_by_func (self->host_entry, on_host_entry_text_changed_cb, self);
-  gtk_editable_set_text (self->host_entry, obs_connection_get_host (self->connection));
-  g_signal_handlers_unblock_by_func (self->host_entry, on_host_entry_text_changed_cb, self);
+  gtk_editable_set_text (self->host_row, obs_connection_get_host (self->connection));
 
   g_signal_handlers_block_by_func (self->port_adjustment, on_port_adjustment_value_changed_cb, self);
   gtk_adjustment_set_value (self->port_adjustment, obs_connection_get_port (self->connection));
@@ -339,14 +304,12 @@ obs_connection_settings_class_init (ObsConnectionSettingsClass *klass)
 
   gtk_widget_class_set_template_from_resource (widget_class, "/com/feaneron/Boatswain/plugins/obs-studio/obs-connection-settings.ui");
 
-  gtk_widget_class_bind_template_child (widget_class, ObsConnectionSettings, host_entry);
-  gtk_widget_class_bind_template_child (widget_class, ObsConnectionSettings, password_entry);
+  gtk_widget_class_bind_template_child (widget_class, ObsConnectionSettings, host_row);
   gtk_widget_class_bind_template_child (widget_class, ObsConnectionSettings, password_row);
   gtk_widget_class_bind_template_child (widget_class, ObsConnectionSettings, port_adjustment);
 
-  gtk_widget_class_bind_template_callback (widget_class, on_host_entry_text_changed_cb);
-  gtk_widget_class_bind_template_callback (widget_class, on_password_entry_activated_cb);
-  gtk_widget_class_bind_template_callback (widget_class, on_password_entry_text_changed_cb);
+  gtk_widget_class_bind_template_callback (widget_class, on_host_row_applied_cb);
+  gtk_widget_class_bind_template_callback (widget_class, on_password_row_applied_cb);
   gtk_widget_class_bind_template_callback (widget_class, on_port_adjustment_value_changed_cb);
 }
 
