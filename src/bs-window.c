7ebfb665fd2f6376a4afe559e25b51a9031aac85
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2023-08-15 14:59:26 -0300

    window: Move new profile entry to sidebar

--- src/bs-window.c
+++ src/bs-window.c
@@ -33,7 +33,6 @@ struct _BsWindow
   AdwApplicationWindow  parent_instance;
 
   GtkAdjustment *brightness_adjustment;
-  GtkWidget *create_profile_button;
   GtkMenuButton *devices_menu_button;
   GtkPopover *devices_popover;
   GtkWidget *empty_page;
@@ -41,13 +40,10 @@ struct _BsWindow
   GtkStack *main_stack;
   GtkListBox *profiles_listbox;
   GtkEditable *new_profile_name_entry;
-  GtkMenuButton *profiles_menu_button;
   GtkListBox *stream_decks_listbox;
 
   GBinding *brightness_binding;
   BsStreamDeck *current_stream_deck;
-  GBinding *profile_name_binding;
-  GtkWindow *profiles_dialog;
   gulong active_profile_changed_id;
 };
 
@@ -80,11 +76,13 @@ append_new_profile (BsWindow *self)
   g_autofree char *new_profile_name = NULL;
   GListModel *profiles;
 
-  if (!gtk_widget_get_sensitive (self->create_profile_button))
-    return;
-
   new_profile_name = g_strdup (gtk_editable_get_text (self->new_profile_name_entry));
+  g_assert (new_profile_name != NULL);
+
   g_strstrip (new_profile_name);
+
+  if (g_utf8_strlen (new_profile_name, -1) == 0)
+    return;
 
   new_profile = bs_profile_new_empty (self->current_stream_deck);
   bs_profile_set_name (new_profile, new_profile_name);
@@ -95,7 +93,6 @@ append_new_profile (BsWindow *self)
   bs_stream_deck_load_profile (self->current_stream_deck, new_profile);
 
   gtk_editable_set_text (self->new_profile_name_entry, "");
-  gtk_menu_button_popdown (self->profiles_menu_button);
 }
 
 static void
@@ -106,14 +103,6 @@ update_active_profile (BsWindow *self)
   unsigned int i;
 
   active_profile = bs_stream_deck_get_active_profile (self->current_stream_deck);
-
-  g_clear_pointer (&self->profile_name_binding, g_binding_unbind);
-  self->profile_name_binding = g_object_bind_property (active_profile,
-                                                       "name",
-                                                       self->profiles_menu_button,
-                                                       "label",
-                                                       G_BINDING_SYNC_CREATE);
-
   profiles = bs_stream_deck_get_profiles (self->current_stream_deck);
   for (i = 0; i < g_list_model_get_n_items (profiles); i++)
     {
@@ -127,18 +116,6 @@ update_active_profile (BsWindow *self)
 
       gtk_widget_set_visible (image, profile == active_profile);
     }
-}
-
-static void
-validate_rename_entry (BsWindow *self)
-{
-  g_autofree char *new_name = NULL;
-  gboolean valid;
-
-  new_name = g_strdup (gtk_editable_get_text (self->new_profile_name_entry));
-  valid = new_name != NULL && g_utf8_strlen (g_strstrip (new_name), -1) > 0;
-
-  gtk_widget_set_sensitive (self->create_profile_button, valid);
 }
 
 static void
@@ -253,13 +230,6 @@ create_stream_deck_row_cb (gpointer item,
 }
 
 static void
-on_create_profile_button_clicked_cb (GtkButton *button,
-                                     BsWindow  *self)
-{
-  append_new_profile (self);
-}
-
-static void
 on_current_stream_deck_active_profile_changed_cb (BsStreamDeck *stream_deck,
                                                   GParamSpec   *pspec,
                                                   BsWindow     *self)
@@ -281,8 +251,6 @@ on_device_manager_stream_deck_added_cb (BsDeviceManager *device_manager,
 
   if (g_list_model_get_n_items (G_LIST_MODEL (device_manager)) == 1)
     select_stream_deck (self, stream_deck);
-
-  gtk_widget_set_visible (GTK_WIDGET (self->profiles_menu_button), TRUE);
 }
 
 static void
@@ -292,17 +260,13 @@ on_device_manager_stream_deck_removed_cb (BsDeviceManager *device_manager,
 {
   g_autofree char *page_name = NULL;
   GtkWidget *child;
-  unsigned int n_stream_decks;
 
   page_name = g_strdup_printf ("%p", stream_deck);
   child = gtk_stack_get_child_by_name (self->main_stack, page_name);
 
   gtk_stack_remove (self->main_stack, child);
 
-  n_stream_decks = g_list_model_get_n_items (G_LIST_MODEL (device_manager));
-  gtk_widget_set_visible (GTK_WIDGET (self->profiles_menu_button), n_stream_decks > 0);
-
-  if (n_stream_decks == 0)
+  if (g_list_model_get_n_items (G_LIST_MODEL (device_manager)) == 0)
     gtk_stack_set_visible_child (self->main_stack, self->empty_page);
 }
 
@@ -340,38 +304,10 @@ on_show_about_action_activated_cb (GSimpleAction *action,
 }
 
 static void
-on_manage_profiles_action_activated_cb (GSimpleAction *simple,
-                                        GVariant      *parameter,
-                                        gpointer       user_data)
-{
-  BsWindow *self = BS_WINDOW (user_data);
-
-  g_clear_pointer (&self->profiles_dialog, gtk_window_destroy);
-
-  if (!self->current_stream_deck)
-    return;
-
-  self->profiles_dialog = GTK_WINDOW (bs_profiles_dialog_new (self->current_stream_deck));
-  gtk_window_set_transient_for (self->profiles_dialog, GTK_WINDOW (self));
-  gtk_window_present (self->profiles_dialog);
-
-  g_object_add_weak_pointer (G_OBJECT (self->profiles_dialog),
-                             (gpointer *) &self->profiles_dialog);
-}
-
-static void
 on_new_profile_name_entry_activate_cb (GtkEntry *entry,
                                        BsWindow *self)
 {
   append_new_profile (self);
-}
-
-static void
-on_new_profile_name_entry_text_changed_cb (GtkEntry   *entry,
-                                           GParamSpec *pspec,
-                                           BsWindow   *self)
-{
-  validate_rename_entry (self);
 }
 
 static void
@@ -388,7 +324,6 @@ on_profiles_listbox_row_activated_cb (GtkListBox    *listbox,
   bs_stream_deck_load_profile (self->current_stream_deck, profile);
 
   update_active_profile (self);
-  gtk_menu_button_popdown (self->profiles_menu_button);
 }
 
 static void
@@ -454,8 +389,6 @@ bs_window_constructed (GObject *object)
           select_stream_deck (self, stream_deck);
           first = FALSE;
         }
-
-      gtk_widget_set_visible (GTK_WIDGET (self->profiles_menu_button), TRUE);
     }
 
   gtk_list_box_bind_model (self->stream_decks_listbox,
@@ -535,7 +468,6 @@ bs_window_class_init (BsWindowClass *klass)
   gtk_widget_class_set_template_from_resource (widget_class, "/com/feaneron/Boatswain/bs-window.ui");
 
   gtk_widget_class_bind_template_child (widget_class, BsWindow, brightness_adjustment);
-  gtk_widget_class_bind_template_child (widget_class, BsWindow, create_profile_button);
   gtk_widget_class_bind_template_child (widget_class, BsWindow, devices_menu_button);
   gtk_widget_class_bind_template_child (widget_class, BsWindow, devices_popover);
   gtk_widget_class_bind_template_child (widget_class, BsWindow, empty_page);
@@ -543,12 +475,9 @@ bs_window_class_init (BsWindowClass *klass)
   gtk_widget_class_bind_template_child (widget_class, BsWindow, main_stack);
   gtk_widget_class_bind_template_child (widget_class, BsWindow, new_profile_name_entry);
   gtk_widget_class_bind_template_child (widget_class, BsWindow, profiles_listbox);
-  gtk_widget_class_bind_template_child (widget_class, BsWindow, profiles_menu_button);
   gtk_widget_class_bind_template_child (widget_class, BsWindow, stream_decks_listbox);
 
-  gtk_widget_class_bind_template_callback (widget_class, on_create_profile_button_clicked_cb);
   gtk_widget_class_bind_template_callback (widget_class, on_new_profile_name_entry_activate_cb);
-  gtk_widget_class_bind_template_callback (widget_class, on_new_profile_name_entry_text_changed_cb);
   gtk_widget_class_bind_template_callback (widget_class, on_profiles_listbox_row_activated_cb);
   gtk_widget_class_bind_template_callback (widget_class, on_stream_decks_listbox_row_activated_cb);
 
@@ -560,7 +489,6 @@ bs_window_init (BsWindow *self)
 {
   const GActionEntry actions[] = {
     { "about", on_show_about_action_activated_cb, },
-    { "manage-profiles", on_manage_profiles_action_activated_cb, },
   };
 
   gtk_widget_init_template (GTK_WIDGET (self));
